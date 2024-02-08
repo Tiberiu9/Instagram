f6c12e01ce1de5aa65828457cdfd309a604048cc
Tibi <tyby_tiberiu@yahoo.com>
2024-02-08 20:27:17 +0200

    Add commenting ability to the post views.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: %i[ create ]
+  before_action :set_post, only: [ :create ]
 
   def create
     @comment = @post.comments.create(user: current_user, body: params[:comment_body])
+    
+    respond_to do |format|
+      format.turbo_stream do 
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+          partial: "posts/post_comments",
+          locals: {post: @post}
+        )
+      end
+    end
   end
 
   def destroy
@@ -16,7 +26,7 @@ class CommentsController < ApplicationController
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
   end
   
 end
