8b98538440630dd3a6b3c0b11d1785f8f4682fc4
Tibi <tyby_tiberiu@yahoo.com>
2024-02-20 00:55:40 +0200

    Add Follow system Controller Logic and afferent routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :posts
   has_one_attached :profile_pic
-  
+
   has_many :likes
 
   has_many :comments
@@ -19,8 +19,23 @@ class User < ApplicationRecord
 
   # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
-  # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
+  has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
-  has_many :following, through: :accepted_send_requests, source: :followed
+  has_many :followings, through: :accepted_sent_requests, source: :followed
+
+  # current_user.follow(User.first)
+  def follow(user)
+    Follow.create(follower: self, followed: user)
+  end
+
+  # current_user.unfollow(user)
+  def unfollow(user)
+    self.accepted_sent_requests.find_by(followed: user)&.destroy
+  end
+
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
+  end
+
 end
