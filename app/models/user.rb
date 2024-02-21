b72de5941278084d086c7f43cce89ab78dfe28be
Tibi <tyby_tiberiu@yahoo.com>
2024-02-21 02:34:20 +0200

    Add Follow, unfollow and cancel-sent-follow-request.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,19 +17,20 @@ class User < ApplicationRecord
 
   has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: "follower_id"
 
+  has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
   # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
-  has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
+  has_many :waiting_followings, through: :waiting_sent_requests, source: :followed
 
   # current_user.follow(User.first)
   def follow(user)
     Follow.create(follower: self, followed: user)
   end
 
-  # current_user.unfollow(user)
+  # current_user.unfollow(user id)
   def unfollow(user)
     self.accepted_sent_requests.find_by(followed: user)&.destroy
   end
