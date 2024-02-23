e42debfec066834282164fb4a6d21fba1fc58661
Tibi <tyby_tiberiu@yahoo.com>
2024-02-23 21:45:12 +0200

    Improved posts feed + accept follow routing error fix.

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,7 +4,7 @@ class FollowsController < ApplicationController
 
   def follow
     current_user.follow(@user)
-    redirect_back_or_to posts_path
+    redirect_back_or_to root_path
   end
 
   def cancel_request
@@ -25,7 +25,6 @@ class FollowsController < ApplicationController
   def decline_follow
     @follow_req.destroy
     redirect_back_or_to root_path
-    # redirect_back(fallback_location: root_path)
   end
 
   private
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,17 @@ class HomeController < ApplicationController
   before_action :set_suggestions
   before_action :set_feeds
   def index
-    # show all post
-    @posts = Post.all
-    # show current user posts
+    ## show all post
+    # @posts = Post.all
+    ## show current user posts
     # @posts = current_user.posts if user_signed_in?
   end
 
   private
+
+  def set_feeds
+    @feeds = Post.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
+    end
 
   def set_suggestions
     @suggestions = [current_user.followers] 
